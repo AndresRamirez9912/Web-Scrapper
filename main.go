d1b49cc73fd72e1b24d78042ef665d505e539d95
Andres Ramirez <85534471+AndresRamirez9912@users.noreply.github.com>
2023-05-01 18:41:39 -0500

    Create postgres connection (#6)

--- main.go
+++ main.go
@@ -3,12 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+	"webScraper/src/database"
 	"webScraper/src/handlers"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
+	// Check If the DB exist
+	exist := database.ChecWebScrapingExist()
+	if !exist {
+		// Create general Connection
+		DB, err := database.CreateConnectionToDatabase("")
+		if err != nil {
+			log.Fatal("Error creating the connection to the DB", err)
+		}
+
+		// Create the tables
+		database.CreateTables(DB)
+	}
+	log.Println("Successfully connection to the database")
+
 	// Create router
 	router := chi.NewRouter()
 
