9d66eef83b8b46589c6b56686ddba46fff4becaa
Andres Ramirez <andres.ramirez9912@gmail.com>
2023-05-09 13:05:28 -0500

    Implement the interface with the Jumbo Scraper

--- src/models/scraping/jumbo.models.go
+++ src/models/scraping/jumbo.models.go
@@ -1,11 +1,32 @@
 package scraping
 
 type JumboProduct struct {
+	Id           string
 	Name         string
 	CurrentPrice string
 	Disccount    string
 	HighPrice    string
+	ProductURL   string
 	Brand        string
 	Description  map[int]string
 	ImageURL     string
+}
+
+func (A *JumboProduct) CreateProductStructure(userId string) Product {
+	description := "&"
+	for _, v := range A.Description {
+		description = v + description
+	}
+	return Product{
+		Product_id:      A.Id,
+		User_product_id: userId + A.Id,
+		Name:            A.Name,
+		Brand:           A.Brand,
+		Description:     description,
+		ImageURL:        A.ImageURL,
+		ProductURL:      A.ProductURL,
+		Current_price:   A.CurrentPrice,
+		Discount:        A.Disccount,
+		High_price:      A.HighPrice,
+	}
 }
