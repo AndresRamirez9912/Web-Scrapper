d0b5a343f39f0e1ca6f31784d55329689d9f6f77
Andres Ramirez <andres.ramirez9912@gmail.com>
2023-06-06 16:23:08 -0500

    Fix the store of the log

--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -5,6 +5,6 @@ if nc -z localhost 3000; then
   echo "Service is running."
 else
   echo "Service is not running."
-  ls -l > ./../priceTracking.log
+  ls -l > priceTracking.log
   exit 1  # Return a non-zero exit code to indicate failure
 fi
