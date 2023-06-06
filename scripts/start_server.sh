9832ea623df479b90642f11a21d24d1efd893563
Andres Ramirez <andres.ramirez9912@gmail.com>
2023-06-06 13:12:22 -0500

    Fix typing error

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,10 +2,10 @@
 
 # Remove the previously files
 rm app
-sudo rm nohub.out
+sudo rm nohup.out
 
 # Build the applicatiojn
 go build -o ./app
 
 # Start the server in the background
-nohub ./app &
+nohup ./app &
